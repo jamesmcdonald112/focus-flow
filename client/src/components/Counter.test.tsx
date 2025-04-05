4051ed4bdf66b0af1a37342fbca5ef4fa7e8d240
James McDonald <jamesmcdonald112@yahoo.ie>
2025-04-05 09:33:18 +0100

    counter completed with tests

--- client/src/components/Counter.test.tsx
+++ client/src/components/Counter.test.tsx
@@ -1,18 +1,42 @@
 import Counter from "./Counter";
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import { userEvent } from '@testing-library/user-event'
-
+import userEvent from '@testing-library/user-event' // 🔧 remove the destructuring
 
 describe("Counter", () => {
-    it('counter increase from 0 to 1 when the button is clicked', async () => {
-        const user = userEvent.setup()
-        render(<Counter />)
+  it("increases from 0 to 1 and resets to 0 when buttons are clicked", async () => {
+    const user = userEvent.setup()
+    render(<Counter />)
 
-        expect(screen.getByText('0')).toBeInTheDocument()
+    const counterDisplay = screen.getByText(content => content.includes('focused for 0'))
+    const increaseButton = screen.getByRole('button', { name: /increase/i })
+    const resetButton = screen.getByRole('button', { name: /reset/i })
 
-        
+    expect(counterDisplay).toBeInTheDocument()
+
+    await user.click(increaseButton)
+
+    expect(screen.getByText(/focused for 1/i)).toBeInTheDocument()
+
+    await user.click(resetButton)
+
+    expect(screen.getByText(/focused for 0/i)).toBeInTheDocument()
+  })
+
+  it(`when the counter exceeds 240 minutes, a message appears: "That's 4 hours of focus! Amazing`, async () => {
+    const user = userEvent.setup()
+    render(<Counter />)
+
+    const increaseButton = screen.getByRole('button', { name: /increase/i })
+
+    expect(screen.queryByText(/That's 4 hours of focus! Amazing./i)).not.toBeInTheDocument()
+
+    for(let i = 0; i < 240; i++) {
+      await user.click(increaseButton)
+    }
+
+    expect(screen.getByText(text => text.includes(`That's 4 hours of focus! Amazing`))).toBeInTheDocument()
 
 
-    })
+  })
 })
--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+
 export default function Counter() {
+    const [counter, setCounter] = useState(0)
+
+    const fourHoursMessage = `That's 4 hours of focus! Amazing`
+
     return (
-        <section></section>
+        <section>
+            <p>focused for {counter}</p>
+            <button onClick={() => setCounter(prevCount => prevCount + 1)}>Increase</button>
+            {counter >= 240 && <p>{fourHoursMessage}</p>}
+            <button onClick={() => setCounter(0)}>Reset</button>
+        </section>
     )
 }
